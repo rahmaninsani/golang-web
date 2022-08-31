5d37f5acb747ede207645784104790f42b8e17c0
Rahman Insani <rahmaninsani54@gmail.com>
2022-08-31 23:06:56 +0700

    Learn: HTTP Test

---
+++ http_test.go
@@ -0,0 +1,28 @@
+package golang_web
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Hello World")
+}
+
+func TestHttp(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	assert.Equal(t, "Hello World", bodyString)
+	fmt.Println(bodyString)
+}
