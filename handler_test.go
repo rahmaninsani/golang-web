3d34158514d053347f9ca9a78c3de7756b75b053
Rahman Insani <rahmaninsani54@gmail.com>
2022-08-31 20:43:12 +0700

    Learn: Handler

---
+++ handler_test.go
@@ -0,0 +1,27 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		// Logic
+		_, err := fmt.Fprint(writer, "Hello World")
+		if err != nil {
+			return
+		}
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
